593f00e8fe4667edcde33dcc4c690642fea75872
jungho <jungpark007@naver.com>
2022-01-19 01:58:13 +0900

    상영에 메서드를 추가하라. - reserve 함수 추가 - calculateFee 함수 추가

--- src/main/java/chapter02/Screening.java
+++ src/main/java/chapter02/Screening.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 public class Screening {
     // 상영할 영화
-    private Moive moive;
+    private Movie movie;
     // 순번
     private int sequence;
     // 상영 시작 시간
@@ -20,5 +20,13 @@ public class Screening {
 
     public boolean isSequence(int sequence) {
         return this.sequence == sequence;
+    }
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
     }
 }

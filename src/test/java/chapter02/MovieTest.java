939f0fae234b7af8bd2bc16c8426f2dff65d3633
jungho <jungpark007@naver.com>
2022-01-19 23:00:33 +0900

    MovieTest 에 NonDiscountPolicy 를 추가

--- src/test/java/chapter02/MovieTest.java
+++ src/test/java/chapter02/MovieTest.java
@@ -38,5 +38,11 @@ class MovieTest {
                         new SequenceCondition(2),
                         new PeriodCondition(DayOfWeek.TUESDAY, LocalTime.of(14, 0), LocalTime.of(16, 59))
                 ));
+
+        Movie starWars = new Movie(
+                "스타워즈",
+                Duration.ofMinutes(210),
+                Money.wons(10_000),
+                new NoneDiscountPolicy());
     }
 }

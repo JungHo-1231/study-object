7e0a26531aaba40040a6f4b7937d56f94436d57e
jungho <jungpark007@naver.com>
2022-01-19 15:06:16 +0900

    Movie (영화) 클래스를 구현하라

---
+++ src/main/java/chapter02/Movie.java
@@ -0,0 +1,29 @@
+package chapter02;
+
+
+import lombok.Getter;
+
+import java.time.Duration;
+
+/**
+ * 영화
+ */
+public class Movie {
+    private String title;
+    private Duration duration;
+    @Getter
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration duration, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.duration = duration;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+}
+

39b429955d09483c65a9c6381b6c2a4a0c4dae2d
jungho <jungpark007@naver.com>
2022-01-19 16:19:29 +0900

    할인 조건을 추가하라 - 할인 조건 인터페이스를 추가하였습니다.   - 구현체인 순번 기준 할인 조건을 추가했습니다.   - 구현체인 상영 기간 기준 할인 조건을 추가했습니다.

---
+++ src/main/java/chapter02/PeriodCondition.java
@@ -0,0 +1,36 @@
+package chapter02;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+/**
+ * 기간 할인 조건
+ */
+public class PeriodCondition implements DiscountCondition {
+    /**
+     * 요일
+     */
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /**
+     * 상영 시작 시간이 기간 안에 포함된다면 ture 를 리턴한다.
+     *
+     * @param screening 상영 정보
+     * @return 할인 기준을 만족하면 true.
+     */
+    @Override
+    public boolean isSatisfied(Screening screening) {
+        return screening.getStartTime().getDayOfWeek().equals(dayOfWeek) &&
+                startTime.compareTo(screening.getStartTime().toLocalTime()) <= 0 &&
+                endTime.compareTo(screening.getStartTime().toLocalTime()) >= 0
+                ;
+    }
+}

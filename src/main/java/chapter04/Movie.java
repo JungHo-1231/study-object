0c34c9b0ffe9048e00f337afbc52f4561c218b3c
jungho <jungpark007@naver.com>
2022-01-21 17:50:41 +0900

    chapter 04 Movie 클래스를 추가하라

---
+++ src/main/java/chapter04/Movie.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+import chapter02.DiscountCondition;
+import chapter02.Money;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+}

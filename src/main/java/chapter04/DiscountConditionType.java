3bf68a4aca510eeb7570002ee237fb1f418b8212
jungho <jungpark007@naver.com>
2022-01-21 17:54:25 +0900

    할인 조건 타입 DiscountConditionType 을 정의히라

---
+++ src/main/java/chapter04/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package chapter04;
+
+public enum DiscountConditionType {
+    SEQUENCE, // 순번 조건
+    PERIOD // 기긴 조건
+}
--- src/main/java/chapter04/Movie.java
+++ src/main/java/chapter04/Movie.java
@@ -2,10 +2,14 @@ package chapter04;
 
 import chapter02.DiscountCondition;
 import chapter02.Money;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Duration;
 import java.util.List;
 
+@Getter
+@Setter
 public class Movie {
     private String title;
     private Duration runningTime;

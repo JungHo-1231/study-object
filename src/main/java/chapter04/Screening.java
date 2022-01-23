2fdc8db7675f674a5180ce9922d3780ff29b4dc0
jungho <jungpark007@naver.com>
2022-01-24 02:33:33 +0900

    Screening 에 오퍼레이션 추가

--- src/main/java/chapter04/Screening.java
+++ src/main/java/chapter04/Screening.java
@@ -1,5 +1,6 @@
 package chapter04;
 
+import chapter02.Money;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,4 +14,29 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount){
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
 }

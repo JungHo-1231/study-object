20b60b360ea653d57dee0d31c7c7c10a0f9a7cc3
jungho <jungpark007@naver.com>
2022-01-24 02:26:06 +0900

    Movie에 요금을 계산하는 오퍼레이션 추가

--- src/main/java/chapter04/Movie.java
+++ src/main/java/chapter04/Movie.java
@@ -1,6 +1,5 @@
 package chapter04;
 
-import chapter02.DiscountCondition;
 import chapter02.Money;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,4 +18,29 @@ public class Movie {
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
+
+    // 요금을 계산하는 오퍼레이션
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
+    }
 }

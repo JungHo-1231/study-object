01d1b7da1ff6fd7237bd383ced5a34882c05a8ca
jungho <jungpark007@naver.com>
2022-01-25 17:15:24 +0900

    chapter 6 끝

---
+++ src/main/java/chapter06/Event.java
@@ -0,0 +1,29 @@
+package chapter06;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+
+public class Event {
+    private String subject;
+    private LocalDateTime from;
+    private Duration duration;
+
+    public Event(String subject, LocalDateTime from, Duration duration) {
+        this.subject = subject;
+        this.from = from;
+        this.duration = duration;
+    }
+
+    public boolean isSatisfied(RecurringSchedule schedule) {
+        if (from.getDayOfWeek() != schedule.getDayOfWeek() || !from.toLocalTime().equals(schedule.getFrom()) || !duration.equals(schedule.getDuration())) {
+            reschedule(schedule);
+            return false;
+        }
+        return true;
+    }
+
+    private void reschedule(RecurringSchedule schedule) {
+
+    }
+}
